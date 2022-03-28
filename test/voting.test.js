85a98284f4dd6c1447716fdfaab9b2338890251f
Nathan Lebas <nathanlebas3@gmail.com>
2022-03-28 18:01:17 +0200

    test implementation in progress

--- test/voting.test.js
+++ test/voting.test.js
@@ -1,9 +1,23 @@
+const { BN, expectRevert, expectEvent } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+const Voting = artifacts.require("Voting");
+ 
 
 contract('Voting', function (accounts) {
+  const owner = accounts[0];
+  const voter1 = accounts[1];
 
   beforeEach(async function () {
     this.VotingInstance = await Voting.new();
    });
 
+   
+  it("...add a voter", async function () {
+    await this.VotingInstance.addVoter(voter1);
+
+    expect(await this.VotingInstance.voters[voter1].isRegistered).to.equal(true);
+  });
+
 });
+
+
